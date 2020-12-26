0cd9f20eb47c97a16f59926ab514c2aa440d48ef
Lucy <hao.lucyy@gmail.com>
2020-12-25 22:19:00 -0700

    done ui

--- tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/MainActivity.kt
+++ tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.tutorial.androidApp
 
 import android.os.Bundle
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.tutorial.shared.Greeting
@@ -10,6 +13,7 @@ import com.example.tutorial.shared.M2MSDK
 import com.example.tutorial.shared.cache.DatabaseDriverFactory
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 fun greet(): String {
     return Greeting().greeting()
@@ -21,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var todoRecyclerView: RecyclerView
     private lateinit var progressBarView: FrameLayout
     private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private val todoRvAdapter = TodoRvAdapter(listOf())
 
     override fun onDestroy() {
         super.onDestroy()
@@ -29,9 +34,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        title = "M2M"
+        setContentView(R.layout.activity_main)
+
+        todoRecyclerView = findViewById(R.id.todoListRv)
+        progressBarView = findViewById(R.id.progressBar)
+        swipeRefreshLayout = findViewById(R.id.swipeContainer)
+
+        todoRecyclerView.adapter = todoRvAdapter
+        todoRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            displayToDo(true)
+        }
+        displayToDo(false)
     }
 
     private fun displayToDo(needReload: Boolean) {
-
+        progressBarView.isVisible = true
+        mainScope.launch {
+            kotlin.runCatching {
+                sdk.getItems(needReload)
+            }.onSuccess {
+                todoRvAdapter.todos = it
+                todoRvAdapter.notifyDataSetChanged()
+            }.onFailure {
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+            progressBarView.isVisible = false
+        }
     }
 }
--- tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/TodoRvAdapter.kt
+++ tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/TodoRvAdapter.kt
@@ -1,10 +1,11 @@
 package com.example.tutorial.androidApp
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tutorial.shared.entity.Todo
 
-class TodoRvAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoViewHolder>() {
+class TodoRvAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoRvAdapter.TodoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         TODO("Not yet implemented")
@@ -16,9 +17,10 @@ class TodoRvAdapter(var todos: List<Todo>) : RecyclerView.Adapter<TodoViewHolder
         holder.bindData(todos[position])
     }
 
-    inner class TodoViewHolder {
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindData(todo: Todo) {
+            val ctx = itemView.context
 
         }
     }
