050fbc49d6f968f1c718036d878cbfe0abbcb9b8
Lucy <hao.lucyy@gmail.com>
2021-02-19 18:34:37 -0700

    implemented swipy pages - can only go up 600 ms

--- tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/MainActivity.kt
+++ tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.tutorial.androidApp
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.os.Handler
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorial.androidApp.ui.onboarding.WalkThrough
 import com.example.tutorial.shared.Greeting
 
 fun greet(): String {
@@ -11,17 +13,34 @@ fun greet(): String {
 }
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var handBalloonButton: ImageButton
+    private lateinit var handPhoneButton: ImageButton
+    private lateinit var handsPinchButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
             this.supportActionBar!!.hide()
         } catch (e: NullPointerException) {
+            println(e)
         }
-        setContentView(R.layout.walkthrough_template)
-        var imageView: ImageView = findViewById(R.id.hands_img_landing)
-        var textView: TextView = findViewById(R.id.instructional_text)
 
-        imageView.setImageResource(R.drawable.hand_balloon)
-        textView.setText("Balloon")
+        setContentView(R.layout.fragment_landing)
+
+        Handler().postDelayed({
+            setContentView(R.layout.fragment_complete_exercises)
+            handsPinchButton = findViewById(R.id.button_hands_pinch)
+
+            handsPinchButton.setOnClickListener {
+                setContentView(R.layout.fragment_play_interactive_games)
+                handBalloonButton = findViewById(R.id.button_hands_balloon)
+
+                handBalloonButton.setOnClickListener {
+                    setContentView(R.layout.fragment_track_progress)
+                    handPhoneButton = findViewById(R.id.button_hands_phone)
+                    handPhoneButton.setOnClickListener { setContentView(R.layout.fragment_auth) }
+                }
+            }
+        }, 600)
     }
 }
---
+++ tutorial/androidApp/src/main/java/com/example/tutorial/androidApp/ui/onboarding/WalkThrough.kt
@@ -0,0 +1,4 @@
+package com.example.tutorial.androidApp.ui.onboarding
+
+class WalkThrough {
+}
