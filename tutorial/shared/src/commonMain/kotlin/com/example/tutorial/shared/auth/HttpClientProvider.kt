ce4649cbbfe93854f98aeaa1cdd3e3915c9657e0
Roshan <rpawar@fispan.com>
2021-03-04 17:46:07 -0800

    Added providers

---
+++ tutorial/shared/src/commonMain/kotlin/com/example/tutorial/shared/auth/HttpClientProvider.kt
@@ -0,0 +1,22 @@
+package com.example.tutorial.shared.auth
+
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.DEFAULT
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+
+class HttpClientProvider {
+    val httpClient = HttpClient {
+        install(JsonFeature) {
+            serializer = KotlinxSerializer()
+        }
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+    }
+}
+
