6625c3fcda37d4648dc64a82bcc7d91af23bdedb
Lucy <hao.lucyy@gmail.com>
2021-04-03 18:24:32 -0600

    add all button images

--- app/android/src/main/java/com/example/tutorial/android/MainActivity.kt
+++ app/android/src/main/java/com/example/tutorial/android/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.tutorial.android
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorial.android.ui.main.HomeActivity
 import com.example.tutorial.android.ui.onboarding.LandingActivity
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this@MainActivity, LandingActivity::class.java))
+        firebaseAuth = FirebaseAuth.getInstance()
+        val currentUser = firebaseAuth.currentUser
+        if (currentUser != null) {
+            Log.d("CURRENT USER", "There is a user logged in.")
+            startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+        } else {
+            Log.d("CURRENT USER", "There is no user logged in.")
+            startActivity(Intent(this@MainActivity, LandingActivity::class.java))
+        }
     }
 }
